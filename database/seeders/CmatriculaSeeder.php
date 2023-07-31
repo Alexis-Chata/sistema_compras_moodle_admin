36652c2bd7bef128e0aa13b8f42a58aee979adc1
rudranthonny <ing.anthonny.joel@gmail.com>
2023-07-31 12:00:01 -0500

    actjualizar

--- database/seeders/CmatriculaSeeder.php
+++ database/seeders/CmatriculaSeeder.php
@@ -25,5 +25,41 @@ class CmatriculaSeeder extends Seeder
         $n_curso->rol = 5;
         $n_curso->save();
 
+        #estudiantes que estan matriculados 5  cursos
+        #estudiante 14
+        $n_curso = new Cmatricula();$n_curso->user_id = 14;$n_curso->modalidad_id = 19;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 14;$n_curso->modalidad_id = 17;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 14;$n_curso->modalidad_id = 14;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 14;$n_curso->modalidad_id = 13;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 14;$n_curso->modalidad_id = 11;$n_curso->rol = 5;$n_curso->save();
+        #estudiante 15
+        $n_curso = new Cmatricula();$n_curso->user_id = 15;$n_curso->modalidad_id = 19;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 15;$n_curso->modalidad_id = 17;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 15;$n_curso->modalidad_id = 14;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 15;$n_curso->modalidad_id = 13;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 15;$n_curso->modalidad_id = 11;$n_curso->rol = 5;$n_curso->save();
+
+        #estudiante 16
+        $n_curso = new Cmatricula();$n_curso->user_id = 16;$n_curso->modalidad_id = 19;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 16;$n_curso->modalidad_id = 17;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 16;$n_curso->modalidad_id = 14;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 16;$n_curso->modalidad_id = 13;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 16;$n_curso->modalidad_id = 11;$n_curso->rol = 5;$n_curso->save();
+
+        #estudiante 17
+        $n_curso = new Cmatricula();$n_curso->user_id = 17;$n_curso->modalidad_id = 19;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 17;$n_curso->modalidad_id = 17;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 17;$n_curso->modalidad_id = 14;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 17;$n_curso->modalidad_id = 13;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 17;$n_curso->modalidad_id = 11;$n_curso->rol = 5;$n_curso->save();
+
+        #estudiante 18
+        $n_curso = new Cmatricula();$n_curso->user_id = 18;$n_curso->modalidad_id = 19;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 18;$n_curso->modalidad_id = 17;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 18;$n_curso->modalidad_id = 14;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 18;$n_curso->modalidad_id = 13;$n_curso->rol = 5;$n_curso->save();
+        $n_curso = new Cmatricula();$n_curso->user_id = 18;$n_curso->modalidad_id = 11;$n_curso->rol = 5;$n_curso->save();
+
+
     }
 }
