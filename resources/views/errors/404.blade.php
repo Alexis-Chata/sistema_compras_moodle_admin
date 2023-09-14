11cb2173d7a366bb78eac90d02dc99d2ea29817c
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-09-14 13:31:53 -0500

    Avance

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,36 @@
-@extends('errors::minimal')
+@extends('silicon-front.main')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@push('stilos')
+    <link rel="stylesheet" type="text/css" href="{{ asset('silicon-front/silicon/tiny-slider.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('silicon-front/silicon/glightbox.css') }}">
+@endpush
+
+@push('javascripts')
+    <script src="{{ asset('silicon-front/silicon/purecounter_vanilla.js') }}"></script>
+    <script src="{{ asset('silicon-front/silicon/tiny-slider.js') }}"></script>
+    <script src="{{ asset('silicon-front/silicon/glightbox.js') }}"></script>
+@endpush
+
+@section('main-content')
+
+    <main>
+        <section class="pt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <!-- Image -->
+                        <img src="assets/images/element/error404-01.svg" class="h-200px h-md-400px mb-4" alt="">
+                        <!-- Title -->
+                        <h1 class="display-1 text-danger mb-0">404</h1>
+                        <!-- Subtitle -->
+                        <h2>¡Oh no, algo salió mal!</h2>
+                        <!-- info -->
+                        <p class="mb-4">O algo salió mal o esta página ya no existe.</p>
+                        <!-- Button -->
+                        <a href="{{ route('index') }}" class="btn btn-primary mb-0">Ir a Home</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+@stop
--- resources/views/silicon-front/partes/header.blade.php
+++ resources/views/silicon-front/partes/header.blade.php
@@ -10,8 +10,7 @@
             <!-- Logo END -->
 
             <!-- Responsive navbar toggler -->
-            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-animation">
                     <span></span>
@@ -26,7 +25,7 @@
                 <!-- Nav category menu START -->
                 <ul class="navbar-nav navbar-nav-scroll me-auto">
                     <!-- Nav item 1 Demos -->
-                    <li class="nav-item dropdown dropdown-menu-shadow-stacked">
+                    {{-- <li class="nav-item dropdown dropdown-menu-shadow-stacked">
                         <a class="nav-link bg-primary bg-opacity-10 rounded-3 text-primary px-3 py-3 py-xl-0"
                             href="#" id="categoryMenu" data-bs-toggle="dropdown" aria-haspopup="true"
                             aria-expanded="false"><i class="bi bi-ui-radios-grid me-2"></i><span>Category</span></a>
@@ -261,7 +260,7 @@
                             <li> <a class="dropdown-item bg-primary text-primary bg-opacity-10 rounded-2 mb-0"
                                     href="#">View all categories</a></li>
                         </ul>
-                    </li>
+                    </li> --}}
                 </ul>
                 <!-- Nav category menu END -->
 
@@ -269,21 +268,20 @@
                 <ul class="navbar-nav navbar-nav-scroll me-auto">
                     <!-- Nav item 1 Menu -->
                     <li class="nav-item dropdown">
-                        <a class="nav-link {{ Route::currentRouteName() == 'index' ? 'active' : '' }}"
-                            href="{{ route('index') }}">Home</a>
+                        <a class="nav-link {{ Route::currentRouteName() == 'index' ? 'active' : '' }}" href="{{ route('index') }}">Home</a>
                     </li>
                     <!-- Nav item 2 Menu -->
                     <li class="nav-item dropdown">
-                        <a class="nav-link {{ Route::currentRouteName() == 'cursos' ? 'active' : '' }}"
-                            href="{{ route('cursos') }}">Cursos</a>
+                        <a class="nav-link {{ Route::currentRouteName() == 'cursos' ? 'active' : '' }}" href="{{ route('cursos') }}">Cursos</a>
                     </li>
-                    <!-- Nav item 3 Menu -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link {{ Route::currentRouteName() == 'mycursos' ? 'active' : '' }}"
-                            href="{{ route('mycursos') }}">Mis Cursos</a>
-                    </li>
+                    @auth
+                        <!-- Nav item 3 Menu -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link {{ Route::currentRouteName() == 'mycursos' ? 'active' : '' }}" href="{{ route('mycursos') }}">Mis Cursos</a>
+                        </li>
+                    @endauth
                     <!-- Nav item 1 Demos -->
-                    <li class="nav-item dropdown" style="display: none;">
+                    {{-- <li class="nav-item dropdown" style="display: none;">
                         <a class="nav-link dropdown-toggle {{ Route::currentRouteName() == 'index' ? 'active' : '' }}"
                             href="#" id="demoMenu" data-bs-toggle="dropdown" aria-haspopup="true"
                             aria-expanded="false">Demos</a>
@@ -831,7 +829,7 @@
                                 </a>
                             </li>
                         </ul>
-                    </li>
+                    </li> --}}
                 </ul>
                 <!-- Nav Main menu END -->
 
@@ -839,11 +837,8 @@
                 <div class="nav my-3 my-xl-0 px-4 flex-nowrap align-items-center">
                     <div class="nav-item w-100">
                         <form class="position-relative">
-                            <input class="form-control pe-5 bg-transparent" type="search" placeholder="Search"
-                                aria-label="Search">
-                            <button
-                                class="bg-transparent p-2 position-absolute top-50 end-0 translate-middle-y border-0 text-primary-hover text-reset"
-                                type="submit">
+                            <input class="form-control pe-5 bg-transparent" type="search" placeholder="Search" aria-label="Search">
+                            <button class="bg-transparent p-2 position-absolute top-50 end-0 translate-middle-y border-0 text-primary-hover text-reset" type="submit">
                                 <i class="fas fa-search fs-6 "></i>
                             </button>
                         </form>
@@ -860,8 +855,7 @@
             @auth
                 <!-- Profile START -->
                 <div class="dropdown ms-1 ms-lg-0">
-                    <a class="avatar avatar-sm p-0" href="#" id="profileDropdown" role="button"
-                        data-bs-auto-close="outside" data-bs-display="static" data-bs-toggle="dropdown"
+                    <a class="avatar avatar-sm p-0" href="#" id="profileDropdown" role="button" data-bs-auto-close="outside" data-bs-display="static" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         <img class="avatar-img rounded-circle"
                             src="{{ 'https://ui-avatars.com/api/' .
@@ -874,8 +868,7 @@
                                 ]) }}"
                             alt="avatar">
                     </a>
-                    <ul class="dropdown-menu dropdown-animation dropdown-menu-end shadow pt-3"
-                        aria-labelledby="profileDropdown">
+                    <ul class="dropdown-menu dropdown-animation dropdown-menu-end shadow pt-3" aria-labelledby="profileDropdown">
                         <!-- Profile info -->
                         <li class="px-3 mb-3">
                             <div class="d-flex align-items-center">
@@ -902,19 +895,16 @@
                             <hr class="dropdown-divider">
                         </li>
                         <!-- Links -->
-                        <li><a class="dropdown-item" href="{{ route('profile.show') }}"><i
-                                    class="bi bi-person fa-fw me-2"></i>Editar Perfil</a></li>
+                        <li><a class="dropdown-item" href="{{ route('profile.show') }}"><i class="bi bi-person fa-fw me-2"></i>Editar Perfil</a></li>
                         <li><a class="dropdown-item" href="{{ route('dashboard') }}"><i class="bi bi-ui-checks-grid fa-fw me-2"></i>Dashboard</a></li>
                         <li><a class="dropdown-item" href="#"><i class="bi bi-gear fa-fw me-2"></i>Account
                                 Settings</a></li>
                         <li><a class="dropdown-item" href="#"><i class="bi bi-info-circle fa-fw me-2"></i>Help</a>
                         </li>
                         <li>
-                            <form method="POST" action="http://sistema_compras_moodle.test/logout"
-                                x-data="">
+                            <form method="POST" action="http://sistema_compras_moodle.test/logout" x-data="">
                                 @csrf
-                                <button class="dropdown-item bg-danger-soft-hover" href="#"><i
-                                        class="bi bi-power fa-fw me-2"></i>Cerrar Sesion</button>
+                                <button class="dropdown-item bg-danger-soft-hover" href="#"><i class="bi bi-power fa-fw me-2"></i>Cerrar Sesion</button>
                             </form>
                         </li>
                         <li>
@@ -922,11 +912,9 @@
                         </li>
                         <!-- Dark mode options START -->
                         <li>
-                            <div
-                                class="bg-light dark-mode-switch theme-icon-active d-flex align-items-center p-1 rounded mt-2">
+                            <div class="bg-light dark-mode-switch theme-icon-active d-flex align-items-center p-1 rounded mt-2">
                                 <button type="button" class="btn btn-sm mb-0 active" data-bs-theme-value="light">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                        fill="currentColor" class="bi bi-sun fa-fw mode-switch" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun fa-fw mode-switch" viewBox="0 0 16 16">
                                         <path
                                             d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z">
                                         </path>
@@ -934,9 +922,7 @@
                                     </svg> Light
                                 </button>
                                 <button type="button" class="btn btn-sm mb-0" data-bs-theme-value="dark">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                        fill="currentColor" class="bi bi-moon-stars fa-fw mode-switch"
-                                        viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars fa-fw mode-switch" viewBox="0 0 16 16">
                                         <path
                                             d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z">
                                         </path>
@@ -947,9 +933,7 @@
                                     </svg> Dark
                                 </button>
                                 <button type="button" class="btn btn-sm mb-0" data-bs-theme-value="auto">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                        fill="currentColor" class="bi bi-circle-half fa-fw mode-switch"
-                                        viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle-half fa-fw mode-switch" viewBox="0 0 16 16">
                                         <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"></path>
                                         <use href="#"></use>
                                     </svg> Auto
@@ -963,8 +947,7 @@
             @endauth
             @guest
                 <div class="navbar-nav">
-                    <a href="{{ route('login') }}" class="btn btn-sm btn-dark mb-0"><i
-                            class="bi bi-power me-2"></i>Iniciar Sesion</a>
+                    <a href="{{ route('login') }}" class="btn btn-sm btn-dark mb-0"><i class="bi bi-power me-2"></i>Iniciar Sesion</a>
                 </div>
             @endguest
         </div>
