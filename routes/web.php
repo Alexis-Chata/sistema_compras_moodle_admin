c7856b0a59b668d6044f6725e882468cfd96299e
Alexis-chata <alexizz.19.ac@gmail.com>
2024-02-12 23:56:42 -0500

    actualiza

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
+    // return view('welcome');
 });
 
 // Route::get('/', function () {
